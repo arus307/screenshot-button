9b4931d619d579270fb913092fc5cc942e2dc2f2
Arus <57102101+Arus307@users.noreply.github.com>
2023-05-07 17:25:04 +0900

    設定画面からvueを消してjQueryのみに変更

--- src/options/options.js
+++ src/options/options.js
@@ -1,97 +1,84 @@
 
-var vue = new Vue({
-    el: "#app",
+const msgTitle = chrome.i18n.getMessage('options_page_title');
+$("#title").html(msgTitle);
 
-    data: {
-        ctrl: true,
-        alt: true,
-        shift: false,
-        key: 's',
 
-        msgTitle: "",
-        msgCurrentShortcutSettings: "",
-        msgShortcutKey: "",
 
-    },
+const msgShortcutKey = chrome.i18n.getMessage('options_page_shortcut_key');
+$("#msg-shortcut-key").html(msgShortcutKey);
 
-    created: function () {
-        this.msgTitle = chrome.i18n.getMessage('options_page_title');
-        document.title = this.msgTitle;
+var ctrl = false;
+var alt = false;
+var shift = false;
+var key = 'a';
 
-        this.msgShortcutKey = chrome.i18n.getMessage('options_page_shortcut_key');
-    },
+var currentKey = '';
 
-    mounted: function () {
-        var _this = this;
-        //ストレージから設定値を復元する
-        chrome.storage.sync.get(['ctrl', 'alt', 'shift', 'key'], function (result) {
-            if ('ctrl' in result) _this.ctrl = result.ctrl;
-            if ('alt' in result) _this.alt = result.alt;
-            if ('shift' in result) _this.shift = result.shift;
-            if ('key' in result) _this.key = result.key;
-        });
-    },
-
-    methods: {
-        onKeyDown: function (e) {
-
-            //Ctrl/Alt/Shift/Meta(Win)キーは設定させない
-            if (e.key == 'Control'
-                || e.key == 'Alt'
-                || e.key == 'Shift'
-                || e.key == 'Meta'
-            ) {
-                return;
-            }
-
-            this.key = e.key.toLowerCase();
-        },
-        saveOptions: function () {
-            chrome.storage.sync.set({
-                "ctrl": this.ctrl,
-                "shift": this.shift,
-                "alt": this.alt,
-                "key": this.key,
-            });
-        },
-    },
-
-    computed: {
-        shortcut: function () {
-            let shortcut = '';
-
-            if (this.ctrl) {
-                shortcut += 'Ctrl + ';
-            }
-            if (this.alt) {
-                shortcut += 'Alt + ';
-            }
-            if (this.shift) {
-                shortcut += 'Shift + ';
-            }
-            shortcut += this.key;
-
-            return shortcut;
-        }
-
-    },
-
-    watch: {
-        "ctrl": function (_old, _new) {
-            this.saveOptions();
-        },
-        "alt": function (_old, _new) {
-            this.saveOptions();
-        },
-        "shift": function (_old, _new) {
-            this.saveOptions();
-        },
-        "key": function (_old, _new) {
-            this.saveOptions();
-        },
-
-        shortcut: function (_old, _new) {
-            this.msgCurrentShortcutSettings = chrome.i18n.getMessage('options_page_current_shortcut_settings', [_new]);
-        }
+//ストレージから設定値を復元する
+chrome.storage.sync.get(['ctrl', 'alt', 'shift', 'key'], function (result) {
+    if ('ctrl' in result) {
+        ctrl = result.ctrl;
+        $("#input-ctrl").prop('checked', ctrl);
     }
+
+    if ('alt' in result) {
+        alt = result.alt;
+        $("#input-alt").prop('checked', alt);
+    }
+    if ('shift' in result) {
+        shift = result.shift;
+        $("#input-shift").prop('checked', shift);
+    }
+    if ('key' in result) {
+        key = result.key;
+        $("#input-key").val(key);
+    }
+
+    setCurrentKey(ctrl, alt, shift, key);
 });
+
+function setCurrentKey(ctrl, alt, shift, key) {
+    // 画面に反映
+    let shortcut = '';
+    if (this.ctrl) {
+        shortcut += 'Ctrl + ';
+    }
+    if (this.alt) {
+        shortcut += 'Alt + ';
+    }
+    if (this.shift) {
+        shortcut += 'Shift + ';
+    }
+    shortcut += this.key;
+
+    const msgCurrentShortcutSettings = chrome.i18n.getMessage('options_page_current_shortcut_settings', [shortcut]);
+    $('#msg-current-shortcut-settings').html(msgCurrentShortcutSettings);
+}
+
+function saveSettings() {
+    console.log("save settings");
+
+    ctrl = $('#input-ctrl').prop('checked');
+    alt = $('#input-alt').prop('checked');
+    shift = $('#input-shift').prop('checked');
+    key = $('#input-key').val();
+
+    console.log(ctrl);
+    console.log(alt);
+    console.log(shift);
+    console.log(key);
+
+    chrome.storage.sync.set({
+        "ctrl": ctrl,
+        "shift": shift,
+        "alt": alt,
+        "key": key,
+    }).then(() => {
+        console.log("save succeed!");
+        setCurrentKey(ctrl, alt, shift, key);
+    }).catch(() => {
+        console.log("save failed.");
+    });
+}
+
+$('#save-btn').on('click', saveSettings);
